f663699dc1ddb1358d5a0443a41d2cb8743fff83
Voris03 <v.vidus03@gmail.com>
2024-07-18 23:33:58 +0300

    start

---
+++ sprint00/homework_createReactComponent/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  );
+}
+
+function Star() {
+  return <div>star</div>
+}
+
+export default App;
